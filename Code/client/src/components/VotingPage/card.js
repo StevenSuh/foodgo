fb5892b1dc46d3356a69890b49083c2118f29929
Steven Suh <stevenesuh@gmail.com>
2018-03-11 18:49:38 -0700

    Added swipe functionality

--- Code/client/src/components/VotingPage/card.js
+++ Code/client/src/components/VotingPage/card.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Draggable from 'react-draggable';
 import ImgSlide from './imgSlide';
 
 import classes from './styles.css';
@@ -7,9 +8,12 @@ class Card extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { expand: false };
+    this.state = { expand: false, position: { x: 0, y: 0 } };
 
     this.expandCard = this.expandCard.bind(this);
+
+    this.dragStop = this.dragStop.bind(this);
+    this.dragControlled = this.dragControlled.bind(this);
   }
 
   renderFullstar = (
@@ -38,6 +42,41 @@ class Card extends Component {
 
   expandCard(event) {
     this.setState({ ...this.state, expand: !this.state.expand });
+  }
+
+  dragStart() {
+  }
+
+  dragStop(e) {
+    // e.currentTarget.classList.remove(classes.drag);
+    const { x } = this.state.position;
+    const card = this.card;
+    if (Math.abs(x) >= card.clientWidth*0.75) {
+      if (x < 0) {
+        card.setAttribute('animation', 'swipe_no');
+        document.getElementById('choice_no').click();
+      } else {
+        card.setAttribute('animation', 'swipe_yes');
+        document.getElementById('choice_yes').click();
+      }
+      return setTimeout((card) => {
+        card.removeAttribute('animation');
+        this.setState({ position: { x: 0, y: 0 } });
+      }, 275, card);
+    } else {
+      this.setState({ ...this.state, position: { x: 0, y: 0 } });
+    }
+  }
+
+  dragControlled(e, position) {
+    const { x, y } = this.state.position;
+    this.setState({ 
+      ...this.state, 
+      position: {
+        x: x + position.deltaX,
+        y: y + position.deltaY
+      } 
+    });
   }
 
   render() {
@@ -73,43 +112,45 @@ class Card extends Component {
     }
 
     return (
-      <div index={this.props.index} className={`${classes.card_item} ${this.state.expand ? classes.expand : ''}`}>
-        <ImgSlide compPhotos={data.photos} />
+      <Draggable position={this.state.position} onStart={this.dragStart} onStop={this.dragStop} onDrag={this.dragControlled}>
+        <div ref={input => {this.card = input}} index={this.props.index} className={`${classes.card_item} ${this.state.expand ? classes.expand : ''}`}>
+          <ImgSlide compPhotos={data.photos} />
 
-        <div className={classes.card_bottom}>
-          <h2 className={classes.card_title}>
-            {data.name}
-          </h2>
-    
-          <div className={classes.card_ratings_wrapper}>
-            <span>
-              <span style={{ marginRight: '5px' }}>{stars}</span>
-              {data.review_count} Reviews
-            </span>  
-          </div>
-
-          <div className={`${classes.card_hide} ${this.state.expand ? classes.expand : ''}`}>
-            <div className={classes.card_left_aside}>
-              {data.location.display_address.join(', ')}
+          <div className={classes.card_bottom}>
+            <h2 className={classes.card_title}>
+              {data.name}
+            </h2>
+      
+            <div className={classes.card_ratings_wrapper}>
+              <span>
+                <span style={{ marginRight: '5px' }}>{stars}</span>
+                {data.review_count} Reviews
+              </span>  
             </div>
-            <aside className={classes.card_aside}>
-              <div className={classes.card_category}>
-                {`${data.categories[0].title} – ${data.price || 'none'}`}
-              </div>
-            </aside>
-            <p className={classes.card_location}>
-              {Math.round(data.distance/1609.34)} mi
-            </p>
-            {reviews}
-          </div>
-        </div>
 
-        <button className={classes.card_expand}
-          onClick={this.expandCard}
-        >
-          {this.arrow}
-        </button>
-      </div>
+            <div className={`${classes.card_hide} ${this.state.expand ? classes.expand : ''}`}>
+              <div className={classes.card_left_aside}>
+                {data.location.display_address.join(', ')}
+              </div>
+              <aside className={classes.card_aside}>
+                <div className={classes.card_category}>
+                  {`${data.categories[0].title} – ${data.price || 'none'}`}
+                </div>
+              </aside>
+              <p className={classes.card_location}>
+                {Math.round(data.distance/1609.34)} mi
+              </p>
+              {reviews}
+            </div>
+          </div>
+
+          <button className={classes.card_expand}
+            onClick={this.expandCard}
+          >
+            {this.arrow}
+          </button>
+        </div>
+      </Draggable>
     );
   }
 }
--- Code/client/src/index.js
+++ Code/client/src/index.js
@@ -1,10 +1,11 @@
+/* eslint import/no-webpack-loader-syntax: off */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter } from 'react-router-dom';
+import '!style-loader!css-loader!./index.css';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
