632bcd3c96c361851bd8d2b501b67c7af007d80c
Matthew Lo <mamlo@ucsc.edu>
2018-03-11 19:25:11 -0700

    Fixed mobile css

--- Code/client/src/components/LandingPage/roomModal.js
+++ Code/client/src/components/LandingPage/roomModal.js
@@ -33,7 +33,7 @@ export default (props) => {
         <div className={classes.room_modal_wrapper}>
           <h4>Successfully created a room!</h4>
 
-          <span>
+          <span className={classes.copy_link}>
             <span 
               className={classes.room_modal_copyIcon}
               onClick={onCopyClick}
@@ -50,7 +50,7 @@ export default (props) => {
           </span>
 
           <a
-            href={`/${props.compKey}`}
+            href={`/${props.fcompKey}`}
             onClick={onRedirect}  
             className={classes.room_modal_button}
           >
